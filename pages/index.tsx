e30bac43539875e573c641758055b67fa55b4da4
Jori Lindell <jori.lindell@gmail.com>
2021-09-21 16:03:06 +0300

    EsLint and Prettier configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -10,12 +11,11 @@ const Home: NextPage = () => {
         <meta name="description" content="Linked registrations UI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>Linked Registration UI</h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
