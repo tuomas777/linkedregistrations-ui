439ea75457a737c4405c76478d95022684a0f890
Jori Lindell <jori.lindell@gmail.com>
2021-09-24 14:22:45 +0300

    Routing for create enrolment page

---
+++ src/domain/enrolment/CreateEnrolmentPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Container from '../app/layout/container/Container';
+
+const CreateEnrolmentPage: React.FC = () => {
+  return <Container>CREATE ENROLMENT PAGE</Container>;
+};
+
+export default CreateEnrolmentPage;
---
+++ src/pages/registration/[registrationId]/enrolment/create.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+import CreateEnrolmentPage from '../../../../domain/enrolment/CreateEnrolmentPage';
+
+const CreateEnrolment: NextPage = () => <CreateEnrolmentPage />;
+
+export default CreateEnrolment;
