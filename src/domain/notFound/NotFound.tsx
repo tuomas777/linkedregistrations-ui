9fc871d8be559d577d531cd2e90027f0e2828b5e
Jori Lindell <jori.lindell@gmail.com>
2021-10-15 22:18:14 +0300

    Add meta data to 404 page

--- src/domain/notFound/NotFound.tsx
+++ src/domain/notFound/NotFound.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
 import React from 'react';
 
 import ErrorTemplate from '../../common/components/errorTemplate/ErrorTemplate';
@@ -7,10 +8,29 @@ import MainContent from '../app/layout/mainContent/MainContent';
 const NotFound: React.FC = () => {
   const { t } = useTranslation('common');
 
+  const openGraphProperties = {
+    description: t('errorPage.text'),
+    title: t('errorPage.title'),
+  };
   return (
-    <MainContent>
-      <ErrorTemplate text={t('errorPage.text')} title={t('errorPage.title')} />
-    </MainContent>
+    <>
+      <Head>
+        <title>{openGraphProperties.title}</title>
+        <meta name="description" content={openGraphProperties.description} />
+        <meta name="twitter:card" content="summary" />
+        {Object.entries(openGraphProperties)
+          .filter((p) => p)
+          .map(([property, value]) => (
+            <meta key={property} property={`og:${property}`} content={value} />
+          ))}
+      </Head>
+      <MainContent>
+        <ErrorTemplate
+          text={t('errorPage.text')}
+          title={t('errorPage.title')}
+        />
+      </MainContent>
+    </>
   );
 };
 
