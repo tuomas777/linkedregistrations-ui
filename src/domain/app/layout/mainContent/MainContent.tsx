13be1fa51f14694ec7060cba7dd6d68e0e80a555
Jori Lindell <jori.lindell@gmail.com>
2021-10-15 19:32:26 +0300

    Simplify MainContent component

--- src/domain/app/layout/mainContent/MainContent.tsx
+++ src/domain/app/layout/mainContent/MainContent.tsx
@@ -6,50 +6,10 @@ import { MAIN_CONTENT_ID } from '../../../../constants';
 
 interface Props {
   className?: string;
-  duration?: number;
-  // This is mainly for testing purposes to test that the scroll function is called properly
-  onScrollFn?: () => void;
-  offset?: number;
 }
 
-const MainContent: React.FC<Props> = ({
-  children,
-  className,
-  duration = 100,
-  offset = -130,
-  onScrollFn,
-}) => {
-  const { asPath } = useRouter();
+const MainContent: React.FC<Props> = ({ children, className }) => {
   const mainContent = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    if (asPath.includes(`#${MAIN_CONTENT_ID}`)) {
-      const scrollToContent = () => {
-        if (onScrollFn) {
-          onScrollFn();
-        } else {
-          scroller.scrollTo(MAIN_CONTENT_ID, {
-            delay: 0,
-            duration: duration,
-            offset: offset,
-            smooth: true,
-          });
-        }
-      };
-
-      const setFocusToFirstFocusable = () => {
-        const focusable = mainContent.current?.querySelectorAll(
-          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-        );
-        if (focusable?.[0]) {
-          (focusable[0] as HTMLElement).focus();
-        }
-      };
-
-      scrollToContent();
-      setTimeout(() => setFocusToFirstFocusable(), duration);
-    }
-  }, [duration, asPath, offset, onScrollFn]);
 
   return (
     <main className={className} id={MAIN_CONTENT_ID} ref={mainContent}>
