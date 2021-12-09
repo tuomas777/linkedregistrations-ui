cac57d4d83e1e2e92ea14b129564f9ac07fa796b
Jori Lindell <jori.lindell@gmail.com>
2021-12-09 17:16:48 +0200

    Make sure that enrolment is not closed because on date in causing false negative test result

--- src/domain/enrolment/registrationWarning/__tests__/RegistrationWarning.test.tsx
+++ src/domain/enrolment/registrationWarning/__tests__/RegistrationWarning.test.tsx
@@ -1,3 +1,5 @@
+import addDays from 'date-fns/addDays';
+import subDays from 'date-fns/subDays';
 import React from 'react';
 
 import { fakeRegistration } from '../../../../utils/mockDataUtils';
@@ -9,11 +11,16 @@ const renderComponent = (registration: Registration) =>
   render(<RegistrationWarning registration={registration} />);
 
 test('should show warning if registration is full', async () => {
+  const now = new Date();
+  const enrolment_start_time = subDays(now, 1).toISOString();
+  const enrolment_end_time = addDays(now, 1).toISOString();
   const registration = fakeRegistration({
     current_attendee_count: 10,
+    current_waiting_list_count: 5,
+    enrolment_end_time,
+    enrolment_start_time,
     maximum_attendee_capacity: 10,
     waiting_list_capacity: 5,
-    current_waiting_list_count: 5,
   });
 
   renderComponent(registration);
