eb084e6a227e7587d345a219f3f2c9bf7cf7f31e
Jori Lindell <jori.lindell@gmail.com>
2022-11-11 15:09:11 +0200

    Disabled date picker while running tests to improve performance

--- src/common/components/dateInput/DateInput.tsx
+++ src/common/components/dateInput/DateInput.tsx
@@ -1,8 +1,10 @@
 import { DateInput as BaseDateInput, DateInputProps } from 'hds-react';
 import React from 'react';
 
+import isTestEnv from '../../../utils/isTestEnv';
+
 const DateInput: React.FC<DateInputProps> = (props) => {
-  return <BaseDateInput {...props} />;
+  return <BaseDateInput disableDatePicker={isTestEnv} {...props} />;
 };
 
 export default DateInput;
