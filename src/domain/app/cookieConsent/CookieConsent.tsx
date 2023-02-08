ae551037cbc3290707a4d43677cef41c81296364
Jori Lindell <jori.lindell@gmail.com>
2023-02-09 01:11:38 +0200

    Remove commented code

--- src/domain/app/cookieConsent/CookieConsent.tsx
+++ src/domain/app/cookieConsent/CookieConsent.tsx
@@ -63,25 +63,6 @@ const CookieConsent: FC = () => {
           ],
         },
         language: { onLanguageChange },
-        onAllConsentsGiven: (consents) => {
-          if (consents.matomo) {
-            //  start tracking
-            // window._paq.push(['setConsentGiven']);
-            // window._paq.push(['setCookieConsentGiven']);
-          }
-        },
-        onConsentsParsed: (consents) => {
-          /* istanbul ignore next */
-          if (consents.matomo === undefined) {
-            // tell matomo to wait for consent:
-            // window._paq.push(['requireConsent']);
-            // window._paq.push(['requireCookieConsent']);
-          } else if (consents.matomo === false) {
-            // tell matomo to forget conset
-            // window._paq.push(['forgetConsentGiven']);
-          }
-        },
-
         focusTargetSelector: `#${PAGE_HEADER_ID}`,
       }}
     />
