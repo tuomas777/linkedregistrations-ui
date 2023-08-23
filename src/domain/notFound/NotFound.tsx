ff80f2ae5d00722aed386c989f6d97414cffae3c
Jori Lindell <jori.lindell@gmail.com>
2023-08-23 15:15:36 +0300

    Common ErrorPageMeta component to remove duplicate code

--- src/domain/notFound/NotFound.tsx
+++ src/domain/notFound/NotFound.tsx
@@ -1,29 +1,20 @@
 import { useTranslation } from 'next-i18next';
-import Head from 'next/head';
 import React from 'react';
 
 import ErrorTemplate from '../../common/components/errorTemplate/ErrorTemplate';
+import ErrorPageMeta from '../../common/components/errrorPageMeta/ErrorPageMeta';
 import MainContent from '../app/layout/mainContent/MainContent';
 
 const NotFound: React.FC = () => {
   const { t } = useTranslation('common');
 
-  const openGraphProperties = {
-    description: t('errorPage.text'),
-    title: t('errorPage.title'),
-  };
   return (
     <>
-      <Head>
-        <title>{openGraphProperties.title}</title>
-        <meta name="description" content={openGraphProperties.description} />
-        <meta name="twitter:card" content="summary" />
-        {Object.entries(openGraphProperties)
-          .filter((p) => p)
-          .map(([property, value]) => (
-            <meta key={property} property={`og:${property}`} content={value} />
-          ))}
-      </Head>
+      <ErrorPageMeta
+        description={t('errorPage.text')}
+        title={t('errorPage.title')}
+      />
+
       <MainContent>
         <ErrorTemplate
           text={t('errorPage.text')}
--- src/domain/signInRequired/SignInRequired.tsx
+++ src/domain/signInRequired/SignInRequired.tsx
@@ -1,32 +1,23 @@
 import { signIn } from 'next-auth/react';
 import { useTranslation } from 'next-i18next';
-import Head from 'next/head';
 import React from 'react';
 
 import Button from '../../common/components/button/Button';
 import ErrorTemplate from '../../common/components/errorTemplate/ErrorTemplate';
+import ErrorPageMeta from '../../common/components/errrorPageMeta/ErrorPageMeta';
 import MainContent from '../app/layout/mainContent/MainContent';
 import styles from './signInRequired.module.scss';
 
 const SignInRequired: React.FC = () => {
   const { t } = useTranslation('common');
 
-  const openGraphProperties = {
-    description: t('signInRequired.text'),
-    title: t('signInRequired.title'),
-  };
   return (
     <>
-      <Head>
-        <title>{openGraphProperties.title}</title>
-        <meta name="description" content={openGraphProperties.description} />
-        <meta name="twitter:card" content="summary" />
-        {Object.entries(openGraphProperties)
-          .filter((p) => p)
-          .map(([property, value]) => (
-            <meta key={property} property={`og:${property}`} content={value} />
-          ))}
-      </Head>
+      <ErrorPageMeta
+        description={t('signInRequired.text')}
+        title={t('signInRequired.title')}
+      />
+
       <MainContent>
         <ErrorTemplate
           buttons={
