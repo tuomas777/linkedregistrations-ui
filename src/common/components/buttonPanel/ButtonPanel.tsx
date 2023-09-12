979e995f7f7bf70f862d0bb7e862bb0c2181abd9
Jori Lindell <jori.lindell@gmail.com>
2023-09-12 17:58:02 +0300

    Move back button to bottom of the summary page

---
+++ src/common/components/buttonPanel/ButtonPanel.tsx
@@ -0,0 +1,101 @@
+import classNames from 'classnames';
+import { IconArrowLeft } from 'hds-react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import Button from '../../../common/components/button/Button';
+import { stringOrNull } from '../../../domain/api/types';
+import Container from '../../../domain/app/layout/container/Container';
+import styles from './buttonPanel.module.scss';
+
+export interface ButtonPanelProps {
+  backButtonAriaLabel?: stringOrNull;
+  onBack?: () => void;
+  submitButtons?: React.ReactElement[];
+  withOffset?: boolean;
+}
+
+const SCROLL_OFFSET = 40;
+
+const ButtonPanel: React.FC<ButtonPanelProps> = ({
+  backButtonAriaLabel,
+  onBack,
+  submitButtons,
+  withOffset = true,
+}) => {
+  const buttonPanel = React.useRef<HTMLDivElement>(null);
+  const { t } = useTranslation('common');
+
+  /* istanbul ignore next */
+  const onDocumentFocusin = (event: FocusEvent) => {
+    const target = event.target;
+
+    if (
+      target instanceof HTMLElement &&
+      !buttonPanel.current?.contains(target)
+    ) {
+      const buttonPanelRect = buttonPanel.current?.getBoundingClientRect();
+      const targetRect = target.getBoundingClientRect();
+
+      if (
+        buttonPanelRect &&
+        buttonPanelRect.top < targetRect.bottom &&
+        window.innerHeight === buttonPanelRect.bottom
+      ) {
+        window.scrollBy(
+          0,
+          targetRect.bottom - buttonPanelRect.top + SCROLL_OFFSET
+        );
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('focusin', onDocumentFocusin);
+
+    return () => {
+      document.removeEventListener('focusin', onDocumentFocusin);
+    };
+  });
+
+  return (
+    <div ref={buttonPanel} className={styles.buttonPanel}>
+      <Container
+        className={classNames({ [styles.noOffset]: !withOffset })}
+        withOffset={withOffset}
+      >
+        <div className={styles.formContainer}>
+          <div className={styles.buttonsRow}>
+            <div
+              className={classNames(styles.submitButtons, {
+                [styles.hideOnMobile]: !submitButtons?.length,
+              })}
+            >
+              {submitButtons}
+            </div>
+            <div
+              className={classNames(styles.otherButtons, {
+                [styles.hideOnMobile]: !onBack,
+              })}
+            >
+              {onBack && (
+                <Button
+                  className={classNames(styles.backButton, styles.smallButton)}
+                  iconLeft={<IconArrowLeft aria-hidden />}
+                  fullWidth={true}
+                  onClick={onBack}
+                  type="button"
+                  variant="secondary"
+                >
+                  {backButtonAriaLabel || t('buttonBack')}
+                </Button>
+              )}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ButtonPanel;
