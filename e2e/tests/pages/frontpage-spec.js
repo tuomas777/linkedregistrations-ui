13fdbaaf0fcba7110732d9177441149ce969adb9
Tero Virtanen <105207219+terovirtanen@users.noreply.github.com>
2024-02-13 21:19:25 +0200

    test: initial playwright e2e tests OH-52 (#138)

---
+++ e2e/tests/pages/frontpage-spec.js
@@ -0,0 +1,19 @@
+import { Page, expect, test } from '@playwright/test';
+
+
+test.describe("Frontpage", () => {
+    let page;
+
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+        await page.goto('/');
+    });
+
+
+    test('title', async () => {
+        const pageTitle = await page.title();
+        expect(pageTitle).toContain("Valitettavasti etsimääsi sivua ei löydy");
+        // expect(pageTitle).toContain("Linked Registrations");
+    });
+
+})
