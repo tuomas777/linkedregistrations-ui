c1a3fbe9732fe5b328d738cfa934499580e45221
mikkojamG <mikko.jamia@gofore.com>
2024-10-18 14:42:21 +0300

    fix: singleSelect remove deprecated selection status LINK-2153

--- src/common/components/singleSelect/SingleSelect.tsx
+++ src/common/components/singleSelect/SingleSelect.tsx
@@ -1,13 +1,8 @@
 import classNames from 'classnames';
 import { Select, SingleSelectProps as HdsSingleSelectProps } from 'hds-react';
-import { useTranslation } from 'next-i18next';
 import React from 'react';
 
 import { OptionType } from '../../../types';
-import {
-  getA11ySelectionMessage,
-  getA11yStatusMessage,
-} from '../../../utils/accessibilityUtils';
 import ComboboxLoadingSpinner, {
   ComboboxLoadingSpinnerProps,
 } from '../comboboxLoadingSpinner/ComboboxLoadingSpinner';
@@ -21,19 +16,9 @@ const SingleSelect: React.FC<SingleSelectProps> = ({
   isLoading,
   ...rest
 }) => {
-  const { t } = useTranslation('common');
-
   return (
     <ComboboxLoadingSpinner isLoading={isLoading}>
-      <Select
-        {...rest}
-        className={classNames(className, styles.select)}
-        getA11yStatusMessage={(options) => getA11yStatusMessage(options, t)}
-        getA11ySelectionMessage={
-          /* istanbul ignore next */
-          (options) => getA11ySelectionMessage(options, t)
-        }
-      />
+      <Select {...rest} className={classNames(className, styles.select)} />
     </ComboboxLoadingSpinner>
   );
 };
